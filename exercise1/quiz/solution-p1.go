546a682078457dc94fa72a3f8c5e2ff97421766b
Gabriel Deleon <notprivategabe@gmail.com>
2020-07-31 15:48:02 +0900

    listed steps for code

--- exercise1/quiz/solution-p1.go
+++ exercise1/quiz/solution-p1.go
@@ -1,1 +1,17 @@
 package quiz
+
+// Load csv
+
+// Get limit of questions
+
+// Ask question
+
+// Read input, log if correct
+
+// Get next question
+
+// Evaluate answer
+
+// Reach Limit
+
+// Report grade
