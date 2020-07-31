3bdd1e71545a03c918b0b58cedf2324ad8fbdc16
Gabriel Deleon <notprivategabe@gmail.com>
2020-07-31 16:06:22 +0900

    first pass at csv handling

--- exercise1/quiz/solution-p1.go
+++ exercise1/quiz/solution-p1.go
@@ -1,6 +1,23 @@
 package quiz
 
-// Load csv
+import (
+	"encoding/csv"
+	"io"
+)
+
+// Load into CSV
+
+func LoadCSV(r io.Reader) *Reader {
+	csv := csv.NewReader(r)
+
+	return csv
+}
+
+// Read/Get next question
+func NextQuestion(csv io.Reader) []string {
+	return csv.Read()
+}
+
 
 // Get limit of questions
 
