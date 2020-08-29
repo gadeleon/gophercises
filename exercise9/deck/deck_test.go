192b2a09696b13d3ac17642005551c4f55a90d10
Ken B <eknegg@gmail.com>
2020-08-29 14:50:53 +0900

    banana

--- exercise9/deck/deck_test.go
+++ exercise9/deck/deck_test.go
@@ -1,9 +1,16 @@
 package main
 
+import (
+	// "deck.deck"
+	"fmt"
+	"testing"
+)
 
 // Test for New
+func TestNewDeckLen(t *testing.T) {
+	d := deck.new()
+}
 
 // Length of []Card is 53
 // 13 of each Suite in the Deck
 // 1 Joker in the Deck
-
