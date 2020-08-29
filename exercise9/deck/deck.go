317a5b1ef63f69226612d280cf6506704b55e544
Gabriel Deleon <notprivategabe@gmail.com>
2020-08-29 14:34:19 +0900

    adding deck

---
+++ exercise9/deck/deck.go
@@ -0,0 +1,22 @@
+package main
+
+// Create a Card Type
+
+// Note for me, J - 11, Q - 12, K - 13
+type Card struct {
+	suite string
+	id string
+	value int
+}
+
+// What's in a CARD
+// Suite
+// ID
+// Value
+
+
+
+
+// Create a func New to make a stroebought deck o' cards.
+// 52 cards + 1 Joker in []Card
+
