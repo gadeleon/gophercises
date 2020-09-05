060802ed4cfdd3bc6788d019bc1cade174d79165
Gabriel Deleon <notprivategabe@gmail.com>
2020-09-05 14:28:15 +0900

    added test for 13 of each suite

--- exercise9/deck/deck_test.go
+++ exercise9/deck/deck_test.go
@@ -14,6 +14,26 @@ func TestNewDeckLen(t *testing.T) {
 }
 
 // Test 13 of each suite
+func TestNewDeckSuite(t *testing.T){
+	// okay
+	d := New()
+	c := make(map[string]int, len(d))
+	for _, j := range d {
+		c[j.suite]++
+	}
+	if c["H"] != 13 {
+		t.Errorf("Got %d Hearts, want 13", c["H"])
+	}
+	if c["S"] != 13 {
+		t.Errorf("Got %d Spades, want 13", c["S"])
+	}
+	if c["D"] != 13 {
+		t.Errorf("Got %d Diamonds, want 13", c["D"])
+	}
+	if c["C"] != 13 {
+		t.Errorf("Got %d Clubs, want 13", c["C"])
+	}
+}
 
 // Test for a joker
 
