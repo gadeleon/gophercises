2db1bbe4ede390eb256a277fe9990652008802bd
Gabriel Deleon <notprivategabe@gmail.com>
2020-10-03 12:20:26 +0900

    new New function! Closer to gophercise suggestion

--- exercise9/deck/deck.go
+++ exercise9/deck/deck.go
@@ -1,21 +1,15 @@
 package deck
 
-// Create a Card Type
-import (
-	"errors"
-	"fmt"
-	"math/rand"
-	"sort"
-)
-
 // Refactor for card type
 type Cards struct {
-	type Suit
-	type Rank
+	Suit
+	Rank
 }
 
 type Suit uint8
 
+// We know Suits never change.
+// They can be constants
 const (
 	Spade Suit = iota
 	Diamond
@@ -26,6 +20,8 @@ const (
 
 type Rank uint8
 
+// We know ranks can never change
+// They can be constants
 const (
 	_ Rank = iota
 	Ace
@@ -42,46 +38,43 @@ const (
 	Queen
 	King
 )
+// We want to know the proper range of ranks
+const (
+	minRank = Ace
+	maxRank = King
+)
 
 // Deprecated
 //type Deck struct {
-//	deck []Card
+//	deck []Cards
 //}
 
-// Make N number of decks
-func New(n int) Deck {
-	var d Deck
-
-	for i := 0; i < n; i++ {
-		c := NewStandard()
-		d.deck = append(d.deck, c...)
-	}
-	return d
-}
+//// Make N number of decks
+//func New(n int) Deck {
+//	var d Deck
+//
+//	for i := 0; i < n; i++ {
+//		c := NewStandard()
+//		d.deck = append(d.deck, c...)
+//	}
+//	return d
+//}
 
 // Create a func New to make a store-bought deck o' cards.
-// 52 cards + 2 joker in []card
+// 52 cards + 2 joker in []Cards
 
+// Need range of suits to iterate for card declartion.
+// Think global
+var suits = [...]Suit{Spade, Diamond, Club, Heart}
 // add a function argument of options
-func NewStandard(opts ...func([]Card) []Card) []Card {
-
-	var c []Card
-	suites := []string{"H", "S", "C", "D"}
-	for _, s := range suites {
-		// Add Ace - 10
-		for i := 0; i < 10; i++ {
-			c = append(c, Card{suite: s, id: fmt.Sprint(i), value: i})
+func New(opts ...func([]Cards) []Cards) []Cards {
+	var c []Cards
+	for _, s := range suits {
+		// Add Ace -
+		for rank := minRank; rank <= maxRank; rank++ {
+			c = append(c, Cards{Suit: s, Rank: rank})
 		}
-		// Jack
-		c = append(c, Card{suite: s, id: "11", value: 11})
-		// Queen
-		c = append(c, Card{suite: s, id: "12", value: 12})
-		// King
-		c = append(c, Card{suite: s, id: "13", value: 13})
-
 	}
-	c = append(c, Card{suite: "j", id: "0", value: 0})
-	c = append(c, Card{suite: "J", id: "0", value: 0})
 
 	for _, opt := range opts {
 		c = opt(c)
@@ -89,54 +82,54 @@ func NewStandard(opts ...func([]Card) []Card) []Card {
 
 	return c
 }
-
-func cSort(c []Card) []Card {
-	sort.SliceStable(c, func(i, j int) bool {
-		return c[i].suite < c[j].suite
-	})
-	return c
-}
-
-func (d Deck) Sort() {
-	sort.SliceStable(d.deck, func(i, j int) bool {
-		return d.deck[i].suite < d.deck[j].suite
-	})
-}
-
-func (d Deck) Shuffle() {
-	// TODO: Figure out, where we init the seed
-	// in the general case.
-	rand.Shuffle(len(d.deck), func(i, j int) {
-		d.deck[i], d.deck[j] = d.deck[j], d.deck[i]
-	})
-}
-
-func (d *Deck) AddJokers(J, j int) error {
-	// Make sure J and j are positive.
-	if j < 0 || J < 0 {
-		return errors.New("donuts")
-	}
-
-	for i := 0; i < j; i++ {
-		d.deck = append(d.deck, Card{suite: "j", id: "0", value: 0})
-	}
-
-	for i := 0; i < J; i++ {
-		d.deck = append(d.deck, Card{suite: "J", id: "0", value: 0})
-	}
-
-	return nil
-
-}
-
-func (d *Deck) FilterCard(c Card) error {
-	// a = append(a[:i], a[i+1:]...)
-	for i, j := range d.deck {
-		if j == c {
-			d.deck = append(d.deck[:i], d.deck[i+1:]...)
-		}
-	}
-	return nil
-}
-
-// TODO: Add filter out specific cards from Deck.
+//
+//func cSort(c []Cards) []Cards {
+//	sort.SliceStable(c, func(i, j int) bool {
+//		return c[i].suite < c[j].suite
+//	})
+//	return c
+//}
+//
+//func (d Deck) Sort() {
+//	sort.SliceStable(d.deck, func(i, j int) bool {
+//		return d.deck[i].suite < d.deck[j].suite
+//	})
+//}
+//
+//func (d Deck) Shuffle() {
+//	// TODO: Figure out, where we init the seed
+//	// in the general case.
+//	rand.Shuffle(len(d.deck), func(i, j int) {
+//		d.deck[i], d.deck[j] = d.deck[j], d.deck[i]
+//	})
+//}
+//
+//func (d *Deck) AddJokers(J, j int) error {
+//	// Make sure J and j are positive.
+//	if j < 0 || J < 0 {
+//		return errors.New("donuts")
+//	}
+//
+//	for i := 0; i < j; i++ {
+//		d.deck = append(d.deck, Cards{suite: "j", id: "0", value: 0})
+//	}
+//
+//	for i := 0; i < J; i++ {
+//		d.deck = append(d.deck, Cards{suite: "J", id: "0", value: 0})
+//	}
+//
+//	return nil
+//
+//}
+//
+//func (d *Deck) FilterCard(c Card) error {
+//	// a = append(a[:i], a[i+1:]...)
+//	for i, j := range d.deck {
+//		if j == c {
+//			d.deck = append(d.deck[:i], d.deck[i+1:]...)
+//		}
+//	}
+//	return nil
+//}
+//
+//// TODO: Add filter out specific cards from Deck.
--- exercise9/deck/deck_test.go
+++ exercise9/deck/deck_test.go
@@ -1,132 +1,130 @@
 package deck
 
 import (
-	"math/rand"
 	"testing"
 )
 
-//// Test creation of arbitrary count of decks
+// Test creation of arbitrary count of decks
+// Repurposed from old New!!
 func TestNew(t *testing.T) {
-
-	for i := 0; i < 4; i++ {
-		d := New(i)
-		if len(d.deck) != (54 * i) {
-			t.Errorf("Got %d card length, expected %d", len(d.deck), 54*1)
-		}
-	}
-}
-
-// Test 13 of each suite & 2 Jokers,  AKA normal deck
-func TestNewDeckSuite(t *testing.T) {
-	var d = NewStandard()
-	c := make(map[string]int, len(d))
-	for _, j := range d {
-		c[j.suite]++
-	}
-	if c["H"] != 13 {
-		t.Errorf("Got %d Hearts, want 13", c["H"])
-	}
-	if c["S"] != 13 {
-		t.Errorf("Got %d Spades, want 13", c["S"])
-	}
-	if c["D"] != 13 {
-		t.Errorf("Got %d Diamonds, want 13", c["D"])
-	}
-	if c["C"] != 13 {
-		t.Errorf("Got %d Clubs, want 13", c["C"])
-	}
-	if c["J"] != 1 {
-		t.Errorf("Got %d JOKER, want 1", c["J"])
-	}
-	if c["j"] != 1 {
-		t.Errorf("Got %d joker, want 1", c["J"])
-	}
-}
-
-// Sort Test, Just printing out.
-func TestDeckSort(t *testing.T) {
-	// Generate deck
-	d := New(1)
-	c1 := Card{"C", "0", 0}
-	d.Sort()
-
-	if d.deck[0] != c1 {
-		t.Errorf("Got %v expected %v ", d.deck[0], c1)
-
+	d := New()
+	if len(d) != 52 {
+		t.Errorf("Got %d cards, want %s",len(d), "52")
 	}
 
 }
-
-func TestfoDeckSort(t *testing.T) {
-	// Generate deck
-
-	d := NewStandard(cSort)
-	c1 := Card{"C", "0", 0}
-
-	if d[0] != c1 {
-		t.Errorf("Got %v expected %v ", d[0], c1)
-
-	}
-
-}
-
-func TestDeckShuffle(t *testing.T) {
-	// We want deterministic testing for tests.
-	// Explicit declaration of seed 1 even though default
-	rand.Seed(1)
-	// Generate 1 deck
-	d := New(1)
-	// Shuffle
-	d.Shuffle()
-	exp := Card{"S", "7", 7}
-	if d.deck[0] != exp {
-		t.Errorf("Got %v, expected %v", d.deck[0], exp)
-	}
-
-}
-
-func TestNewDeckJokers(t *testing.T) {
-	d := New(1)
-	err := d.AddJokers(1, 1)
-
-	// Sort the deck into suites
-	c := make(map[string]int, len(d.deck))
-	for _, j := range d.deck {
-		c[j.suite]++
-	}
-
-	if c["J"] != 2 {
-		t.Errorf("Got %d JOKER, want 2", c["J"])
-	}
-	if c["j"] != 2 {
-		t.Errorf("Got %d joker, want 2", c["J"])
-	}
-
-	// Check for negative # of jokers.
-	err = d.AddJokers(-1, 1)
-	if err != nil {
-		return
-	} else {
-		t.Errorf("Expected  got %v", err)
-	}
-
-}
-func TestFilterCard(t *testing.T) {
-	d := New(2)
-	oglen := len(d.deck)
-	// Card we want to filter out
-	filtered := Card{"H", "2", 2}
-	d.FilterCard(filtered)
-
-	for _, j := range d.deck {
-		if j == filtered {
-			t.Errorf("%v found, should not be in slice", filtered)
-		}
-	}
-	// Check to see all other expected cards are present
-	total := len(d.deck)
-	if total == oglen {
-		t.Errorf("Original length is %v, should be less than %v", oglen, total)
-	}
-
-}
+//
+//// Test 13 of each suite & 2 Jokers,  AKA normal deck
+//func TestNewDeckSuite(t *testing.T) {
+//	var d = New()
+//	c := make(map[string]int, len(d))
+//	for _, j := range d {
+//		c[j.suite]++
+//	}
+//	if c["H"] != 13 {
+//		t.Errorf("Got %d Hearts, want 13", c["H"])
+//	}
+//	if c["S"] != 13 {
+//		t.Errorf("Got %d Spades, want 13", c["S"])
+//	}
+//	if c["D"] != 13 {
+//		t.Errorf("Got %d Diamonds, want 13", c["D"])
+//	}
+//	if c["C"] != 13 {
+//		t.Errorf("Got %d Clubs, want 13", c["C"])
+//	}
+//	if c["J"] != 1 {
+//		t.Errorf("Got %d JOKER, want 1", c["J"])
+//	}
+//	if c["j"] != 1 {
+//		t.Errorf("Got %d joker, want 1", c["J"])
+//	}
+//}
+//
+//// Sort Test, Just printing out.
+//func TestDeckSort(t *testing.T) {
+//	// Generate deck
+//	d := New(1)
+//	c1 := Card{"C", "0", 0}
+//	d.Sort()
+//
+//	if d.deck[0] != c1 {
+//		t.Errorf("Got %v expected %v ", d.deck[0], c1)
+//
+//	}
+//
+//}
+//
+//func TestfoDeckSort(t *testing.T) {
+//	// Generate deck
+//
+//	d := NewStandard(cSort)
+//	c1 := Card{"C", "0", 0}
+//
+//	if d[0] != c1 {
+//		t.Errorf("Got %v expected %v ", d[0], c1)
+//
+//	}
+//
+//}
+//
+//func TestDeckShuffle(t *testing.T) {
+//	// We want deterministic testing for tests.
+//	// Explicit declaration of seed 1 even though default
+//	rand.Seed(1)
+//	// Generate 1 deck
+//	d := New(1)
+//	// Shuffle
+//	d.Shuffle()
+//	exp := Card{"S", "7", 7}
+//	if d.deck[0] != exp {
+//		t.Errorf("Got %v, expected %v", d.deck[0], exp)
+//	}
+//
+//}
+//
+//func TestNewDeckJokers(t *testing.T) {
+//	d := New(1)
+//	err := d.AddJokers(1, 1)
+//
+//	// Sort the deck into suites
+//	c := make(map[string]int, len(d.deck))
+//	for _, j := range d.deck {
+//		c[j.suite]++
+//	}
+//
+//	if c["J"] != 2 {
+//		t.Errorf("Got %d JOKER, want 2", c["J"])
+//	}
+//	if c["j"] != 2 {
+//		t.Errorf("Got %d joker, want 2", c["J"])
+//	}
+//
+//	// Check for negative # of jokers.
+//	err = d.AddJokers(-1, 1)
+//	if err != nil {
+//		return
+//	} else {
+//		t.Errorf("Expected  got %v", err)
+//	}
+//
+//}
+//func TestFilterCard(t *testing.T) {
+//	d := New(2)
+//	oglen := len(d.deck)
+//	// Card we want to filter out
+//	filtered := Card{"H", "2", 2}
+//	d.FilterCard(filtered)
+//
+//	for _, j := range d.deck {
+//		if j == filtered {
+//			t.Errorf("%v found, should not be in slice", filtered)
+//		}
+//	}
+//	// Check to see all other expected cards are present
+//	total := len(d.deck)
+//	if total == oglen {
+//		t.Errorf("Original length is %v, should be less than %v", oglen, total)
+//	}
+//
+//}
