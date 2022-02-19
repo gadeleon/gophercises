2cc9d1071cff4eef4ade6fdb70bbf0a59de8a552
Gabriel Deleon <notprivategabe@gmail.com>
2022-02-19 17:41:27 +0900

    first pass at new quiz and test

--- gophercise1/internal/quiz/quiz.go
+++ gophercise1/internal/quiz/quiz.go
@@ -1,16 +1,76 @@
 package quiz
 
+import (
+	"encoding/csv"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
 type Question struct {
 	Question string
 	Answer   string
 }
 
 type Quiz struct {
-	Source    string
-	Questions []Question
+	Questions []*Question
 	Score     int
 }
 
 // Open CSV File
 // Create Quiz with Questions
 // Go through Quiz.Questions, and ask Question. Score ++ if correct
+
+// func matchName(filename string) filepath.WalkFunc {
+// 	return func(path string, info fs.FileInfo, err error) error {
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if info.Name() == filename {
+// 		filename = info.
+// 	}
+// 	return nil
+// }
+
+func NewQuiz(folder string, filename string) (*Quiz, error) {
+	// find the file
+	qs := make([]*Question, 0)
+	walk := filepath.WalkDir(folder, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.Name() == filename {
+			// Make the filepath to open
+			fp := folder + "/" + filename
+			inf, err := os.Open(fp)
+			if err != nil {
+				return err
+			}
+			defer inf.Close()
+			// open as csv
+			csvReader := csv.NewReader(inf)
+			for {
+				rec, err := csvReader.Read()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					return err
+				}
+				qs = append(qs, &Question{rec[0], rec[1]})
+
+			}
+		}
+		return nil
+	})
+	if walk != nil {
+		return nil, walk
+	}
+	quiz := &Quiz{
+		qs,
+		0,
+	}
+	return quiz, nil
+
+}
--- gophercise1/internal/quiz/quiz_test.go
+++ gophercise1/internal/quiz/quiz_test.go
@@ -1,1 +1,38 @@
 package quiz
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewQuiz(t *testing.T) {
+	type args struct {
+		folder   string
+		filename string
+	}
+	tArgs := args{"../../../gophercise1/assets", "test_question.csv"}
+	expected := &Quiz{
+		[]*Question{{"1+1", "2"}},
+		0,
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Quiz
+		wantErr bool
+	}{
+		{"Happy Path", tArgs, expected, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewQuiz(tt.args.folder, tt.args.filename)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewQuiz() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewQuiz() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
